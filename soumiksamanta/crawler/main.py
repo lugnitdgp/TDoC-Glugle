31d0ca93501fca7b734adaa9456e1dd1b8547122
soumiksamanta007 <soumiksamantaa@gmail.com>
2021-10-27 21:39:12 +0530

    completed task 4 - query preprocessing and ranking

--- soumiksamanta/crawler/main.py
+++ soumiksamanta/crawler/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_paginate import Pagination, get_page_args
 import pymongo
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
 
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
@@ -21,15 +26,28 @@ def get_paginated_search_results(search_results, offset=0, per_page=10):
     return search_results[offset: offset + per_page]
 
 
-@app.route("/search")
-def search():
+def get_query_keywords(query):
+    # lowercasing
+    query = query.lower()
 
-    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    # remove punctuations
+    translator = str.maketrans('', '', string.punctuation)
+    query = query.translate(translator)
+    
+    # removing stopwords and tokenization
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(query)
+    filtered_query = [word for word in word_tokens if word not in stop_words]
 
-    query = request.args.get('query', "")
-    search_results = []
+    # perform stemming
+    stemmer = PorterStemmer()
+    word_tokens = word_tokenize(query)
+    stems = [stemmer.stem(word) for word in word_tokens]
+    return stems
 
-    result_data = db.query_data.find(
+
+def query_database(query):
+    return db.query_data.find(
         {
             '$text' : {
                 '$search' : query,
@@ -48,6 +66,9 @@ def search():
         ]
     )
 
+
+def remove_duplicates(result_data):
+    search_results = []
     for doc in result_data:
         exist = False
         for result in search_results:
@@ -57,29 +78,48 @@ def search():
 
         if exist == False:
             search_results.append(doc)
-
-    # search_results = db.query_data.find(
-    #     {
-    #         '$text' : {
-    #             '$search' : query,
-    #             '$caseSensitive' : False,
-    #         }
-    #     },
-    #     {
-    #         'score': {
-    #             '$meta': "textScore"
-    #         }
-    #     }
-    # ).sort(
-    #     [
-    #         ('score', {'$meta': 'textScore'}),
-    #         ('_id', pymongo.DESCENDING)
-    #     ]
-    # ).skip(offset).limit(per_page)
+    return search_results
 
 
+def sort_rank(search_results, keywords):
+    for result in search_results:
+        for word in keywords:
+            if word in result['title']:
+                result['score'] += 2
+            else:
+                result['score'] += 0
+            if word in result['description']:
+                result['score'] += 1
+            else:
+                result['score'] += 0
+    return sorted(search_results, key = lambda result: result['score'], reverse=True)
+
+
+@app.route("/search")
+def search():
+
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+
+    query = request.args.get('query', "")
+    search_results = []
+
+    # preprocess query
+    keywords = get_query_keywords(query)
+    processed_query = " ".join(keywords)
+
+    # search for data in database
+    search_results = query_database(processed_query)
+
+    # filter out duplicate search results
+    search_results = remove_duplicates(search_results)
+
+    # rank the retrieved search results
+    search_results = sort_rank(search_results, keywords)
+
+    # find total no of search results
     total = len(search_results)
 
+    # paginate the search results
     pagination = Pagination(page=page,
                             total=total,
                             css_framework='bootstrap4',
