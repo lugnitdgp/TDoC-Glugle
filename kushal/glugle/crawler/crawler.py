645e385846b21ee51ceca7244aadb206ac6b6db5
Kushal Sarda <78421890+kushal-sarda@users.noreply.github.com>
2021-10-28 11:21:44 +0530

    Update crawler.py

--- kushal/glugle/crawler/crawler.py
+++ kushal/glugle/crawler/crawler.py
@@ -1,16 +1,16 @@
-import pymongo
-import urllib.parse
 import requests
-import pymongo
 from bs4 import BeautifulSoup
+import urllib.parse
+import urllib.request
+import pymongo
+import lxml
 import sys
-from html.parser import HTMLParser
 
 
 class Crawler:
     client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
     db = client.glugledb
-    collection = db.info
+    collection = db.results
 
     disallowed_link = []
 
@@ -21,76 +21,75 @@ class Crawler:
             robot = requests.get(robots)
         except:
             print("Robots not found here!")
-            crawl(self, url, depth)
+            self.crawl(url, depth)
 
-        soup = BeautifulSoup(robot.text, 'html.parser')
-        content = soup.find_all('p')
+        soup = BeautifulSoup(robot.text, 'lxml')
+        content = soup.find('p').text
 
         for word in content:
             if word[0] == '/':
                 self.disallowed_link.append(urllib.parse.urljoin(url, word))
         print("Robots found and appended in dissallowed_links...")
 
-        crawl(self, url, depth, self.disallowed_link)
+        self.crawl(url, depth, self.disallowed_link)
 
-
-def crawl(self, url, depth, *disallowed_link):
-    try:
-        print(f"Crawling url {url} at depth: {depth}")
-        response = requests.get(url)
-    except:
-        print(f"Failed to perform HTTP GET request on {url}")
-        return
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    try:
-        title = soup.find('title').text
-        desc = ''
-
-        for tag in soup.findAll():
-            if tag.name == 'p':
-                desc = desc + tag.text.strip().replace('\n', '')
-
-    except:
-        print("Failed to retrieve title and desc...")
-        return
-
-    query = {
-        'url': url,
-        'title': title,
-        'description': desc,
-    }
-
-    results = self.db.results
-    results.insert_one(query)
-    results.create_index(
-        [
-            ('url', pymongo.TEXT),
-            ('title', pymongo.TEXT),
-            ('desc', pymongo.TEXT)
-        ],
-        name='results',
-        default_language='english'
-    )
-
-    if depth == 0:
-        return
-
-    links = soup.findAll('a')
-
-    for link in links:
+    def crawl(self, url, depth, *disallowed_link):
         try:
-            if link['href'] not in disallowed_link:
-                if 'http' in link['href']:
-                    crawl(self, link['href'], depth-1, disallowed_link)
-                else:
-                    link['href'] = urllib.parse.urljoin(url, link['href'])
-                    crawl(self, link['href'], depth-1, disallowed_link)
-        except KeyError:
-            print("No links retrieved from the page")
-            pass
+            print(f"Crawling url {url} at depth: {depth}")
+            response = requests.get(url)
+        except:
+            print(f"Failed to perform HTTP GET request on {url}")
+            return
+        soup = BeautifulSoup(response.text, 'lxml')
 
-    self.client.close()
+        try:
+            title = soup.find('title').text
+            desc = ''
+
+            for tag in soup.findAll():
+                if tag.name == 'p':
+                    desc = desc + tag.text.strip().replace('\n', '')
+
+        except:
+            print("Failed to retrieve title and desc...")
+            return
+
+        query = {
+            'url': url,
+            'title': title,
+            'description': desc,
+        }
+
+        results = self.db.results
+        results.insert_one(query)
+        results.create_index(
+            [
+                ('url', pymongo.TEXT),
+                ('title', pymongo.TEXT),
+                ('desc', pymongo.TEXT)
+            ],
+            name='results',
+            default_language='english'
+        )
+
+        if depth == 0:
+            return
+
+        links = soup.findAll('a')
+
+        for link in links:
+            try:
+                if link['href'] not in disallowed_link:
+                    if 'http' in link['href']:
+                        self.crawl(link['href'], depth-1, disallowed_link)
+                    else:
+                        link['href'] = urllib.parse.urljoin(url, link['href'])
+                        self.crawl(link['href'], depth-1, disallowed_link)
+            except KeyError:
+                print("No links retrieved from the page")
+                pass
+
+        self.client.close()
 
 
 spider = Crawler()
