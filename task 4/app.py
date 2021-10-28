7d576e7842e072d0248d3e4b6efb8207a1afdd28
Krishna Kumar Choudhury <souravae35@gmail.com>
2021-10-28 21:40:43 +0530

    task 4

---
+++ task 4/app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request
+import pymongo
+import os
+from flask_paginate import Pagination, get_page_args
+from ranking import Ranking
+from query_processing import QueryProcessing
+import time
+
+
+app= Flask(__name__)
+@app.route('/')
+def entry_point():
+    return render_template('homepage.html')
+    
+@app.route('/search_results')
+def search_results():
+
+    client = pymongo.MongoClient('mongodb://127.0.0.1.27017', connect=False)
+    db = client.crawl_database
+    search_string=request.args.get('search_query')
+    processor = QueryProcessing(search_string)
+    keywords = processor.processor()
+
+    query = []
+
+    start = time.time()
+    
+
+    for keyword in keywords:
+        query.extend(db.crawl_collection_gfg.find(
+            {'$text': {'$search': keyword, '$caseSensitive': False}}))
+
+    end = time.time()
+    print(f"time to execute: {end-start}")
+
+    search_result = []
+
+    for doc in query:
+        exist = False
+        for result in search_result:
+            if result['title'] == doc['title'] or result['url'] == doc['url']:
+                exist = True
+                break
+
+        if exist == False:
+            search_result.append(doc)
+
+    rank = Ranking(search_result, search_string)
+
+    ranked_result = rank.sorted_results()
+
+    client.close()
+
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+
+    total = len(ranked_result)
+
+    pagination = Pagination(page=page, per_page=per_page, total=total)
+
+    return render_template('results.html',
+                           search_result=ranked_result[offset:offset+per_page],
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           search_string=search_string
+                           )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
+    
+
+#     return render_template('results.html',results=results)
+
+
+# if __name__=='__main__':
+#     app.run(debug=True)
